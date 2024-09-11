00a7555588c78d487f96863d9df384c07bb32002
joelmendoza1710 <145950401+joelmendoza1710@users.noreply.github.com>
2024-09-11 14:56:58 -0500

    proyectos back & fron

---
+++ safetrabel-api/src/main/java/com/safetrabel/safetrabel_api/SafetrabelApiApplication.java
@@ -0,0 +1,13 @@
+package com.safetrabel.safetrabel_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetrabelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetrabelApiApplication.class, args);
+	}
+
+}
